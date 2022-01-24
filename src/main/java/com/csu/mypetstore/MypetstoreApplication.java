f02708669ad55d3d789dafeef73f19279e800c57
ThunderStones <879068849@qq.com>
2022-01-24 22:32:00 +0800

    initiallize mypetstore

---
+++ src/main/java/com/csu/mypetstore/MypetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.csu.mypetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoreApplication.class, args);
+    }
+
+}
